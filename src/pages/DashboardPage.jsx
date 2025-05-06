f6d4c49ef71bc1ca3c96a88a072daa13b6351c5f
Ajeeth <ajeetheswari14@gmail.com>
2025-05-06 12:13:55 +0530

    test service added

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,26 @@
+import { DashboardPieChart } from "@/components/dashboardCharts/DashboardPieChart";
+import { TestChart } from "@/components/dashboardCharts/TestChart";
 import { useAuth } from "@/contexts/AuthContext";
+import { getUserTasks } from "@/services/taskService";
 import { toTitleCase } from "@/utils/stringUtils";
+import { useEffect } from "react";
 
 const DashboardPage = () => {
   const { userData } = useAuth();
+
+  useEffect(() => {
+    fetchUser();
+  });
+
+  const fetchUser = async () => {
+    const res = await getUserTasks(
+      userData.currentUserLogin,
+      userData.currentUserLogin,
+      userData.clientURL
+    );
+
+    console.log(res);
+  };
 
   return (
     <div className="flex flex-col gap-y-4">
@@ -13,6 +31,9 @@ const DashboardPage = () => {
         <p className="text-gray-400 text-sm">
           here's what's happening with your account today
         </p>
+
+        <DashboardPieChart />
+        <TestChart />
       </div>
     </div>
   );
