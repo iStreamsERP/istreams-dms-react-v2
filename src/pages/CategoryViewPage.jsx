23ad81cc2f904bd7d76ddcfd21ea2c6bd54e3478
Ajeeth <ajeetheswari14@gmail.com>
2025-06-05 18:56:57 +0530

    category search added

--- src/pages/CategoryViewPage.jsx
+++ src/pages/CategoryViewPage.jsx
@@ -1,12 +1,20 @@
 import GlobalSearchInput from "@/components/GlobalSearchInput";
 import { Card } from "@/components/ui/card";
+import { callSoapService } from "@/services/callSoapService";
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 import TimeRangeSelector from "../components/TimeRangeSelector";
 import { useAuth } from "../contexts/AuthContext";
-import { getCategoriesSummary } from "../services/dmsService";
-import { callSoapService } from "@/services/callSoapService";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const CategoryViewPage = () => {
   const { userData } = useAuth();
@@ -15,49 +23,74 @@ const CategoryViewPage = () => {
   const [categories, setCategories] = useState([]);
   const [filterDays, setFilterDays] = useState("365");
   const [loading, setLoading] = useState(true);
+
   const [globalFilter, setGlobalFilter] = useState("");
+  const [filterField, setFilterField] = useState("ALL");
+
+  const buildFilterCond = () => {
+  if (filterField === "ALL" || globalFilter.trim() === "") {
+    return ""; // No filtering
+  }
+  return `${filterField} LIKE '%${globalFilter}%'`;
+};
+
+// console.log(buildFilterCond());
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const payload = {
-          NoOfDays: filterDays,
-          ForTheUser: `${userData.isAdmin ? "" : userData.userName}`,
-        };
-
-        const response = await callSoapService(
-          userData.clientURL,
-          "DMS_GetDashboard_CategoriesSummary",
-          payload
-        );
-
-        setCategories(response);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching dashboard summary:", error);
-      }
-    };
-
     fetchData();
-  }, [filterDays]);
+  }, [filterField, globalFilter]);
+  
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const payload = {
+        NoOfDays: filterDays,
+        ForTheUser: `${userData.isAdmin ? "" : userData.userName}`,
+        FilterCond: buildFilterCond(),
+      };
 
-  const filteredCategories = categories.filter((category) => {
-    const search = globalFilter.toLowerCase();
-    return category.DOC_RELATED_CATEGORY.toLowerCase().includes(search);
-  });
+      const response = await callSoapService(
+        userData.clientURL,
+        "DMS_GetDashboard_CategoriesSummary",
+        payload
+      );
+
+      setCategories(response);
+      
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching dashboard summary:", error);
+    }
+  };
 
   return (
     <div className="space-y-4">
       {/* CONTROLS ROW */}
       <div className="flex flex-col md:flex-row md:justify-between items-stretch gap-2">
         {/* Search */}
-        <div className="w-full lg:w-1/2">
+        <div className="flex items-center gap-2 w-full">
+          <Select value={filterField} onValueChange={setFilterField}>
+            <SelectTrigger className="w-[100px]">
+              <SelectValue placeholder="All" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="ALL">All</SelectItem>
+                <SelectItem value="X_CLIENT_NAME">Client</SelectItem>
+                <SelectItem value="X_VENDOR_NAME">Supplier</SelectItem>
+                <SelectItem value="DOC_REF_VALUE">Doc Ref for</SelectItem>
+                <SelectItem value="DOC_TAGS">Tags</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <div className="w-full lg:w-1/2">
             <GlobalSearchInput
               value={globalFilter}
               onChange={setGlobalFilter}
             />
           </div>
+        </div>
 
         {/* Filter dropdown */}
         <div className="flex-shrink-0">
@@ -69,11 +102,11 @@ const CategoryViewPage = () => {
         <div className="flex justify-center items-center">
           <BarLoader color="#36d399" height={2} width="100%" />
         </div>
-      ) : filteredCategories.length === 0 ? (
+      ) : categories.length === 0 ? (
         <p className="text-center text-sm">No data found...</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-          {filteredCategories.map((category) => (
+          {categories.map((category) => (
             <Link
               to="/document-view"
               state={{ categoryName: category.DOC_RELATED_CATEGORY }}
